b3a38215e8953e67e789502da975c43453a1156a
1mpuser <alex022003@mail.ru>
2021-08-09 14:15:50 +0300

    Semi-Final Commit

--- mask.js
+++ mask.js
@@ -1,11 +1,10 @@
 let inputs = document.querySelectorAll('input');
 let telephone = inputs[0];
 telephone.value = '+7 (___) ___-__-__ ';
-function replacingUnderscoresWithoutLetters(key) {
+function replacingUnderscoresWithoutLetters(elem, key) {
 	//let bool = doWeIncludeLetters;
 	//console.log(key);
-	let boolean;
-	boolean = /\d/.test(key);
+	let boolean = /\d/.test(key);
 	// function returnSpecialKeys() {
 	// 	return (
 	// 		key == 'ArrowLeft' ||
@@ -15,11 +14,13 @@ function replacingUnderscoresWithoutLetters(key) {
 	// 	);
 	// }
 	//returnSpecialKeys();
-	let tmpvalue = telephone.value;
 	if (boolean) {
+		let tmpvalue = elem.value;
 		let value = tmpvalue.replace(/\_/, key);
-		telephone.value = value;
+		elem.value = value;
+		event.preventDefault();
 	}
+	//console.log(elem.value);
 	//if (!bool) let value=tmpvalue.replace(/^\+7\s\(([\d_]{1,3})\)_+\s$/)
 }
 
@@ -29,20 +30,23 @@ telephone.addEventListener('keypress', function (event) {
 		event.key == 'ArrowRight' ||
 		event.key == 'Delete' ||
 		event.key == 'Backspace'
-	) {
-		replacingUnderscoresWithoutLetters(event.key);
-	} else {
+	)
+		replacingUnderscoresWithoutLetters(this, event.key);
+	else {
 		let bool = /\d/.test(event.key);
 		//console.log(bool);
 		if (bool) {
-			replacingUnderscoresWithoutLetters(event.key);
+			replacingUnderscoresWithoutLetters(this, event.key);
+			//console.log(this.value);
 			let index = telephone.value.indexOf('_');
 			if (index == -1) return;
 			else {
-				let range = new Range();
-				range.setStart(this.parentNode, index);
-				range.setEnd(this.parentNode, index);
+				return (this.selectionStart = this.selectionEnd = index);
 			}
 		} else return event.preventDefault();
 	}
+	//console.log(this.value);
 });
+function focusing() {
+	this.selectionStart = this.selectionEnd = 4;
+}
